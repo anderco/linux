d4f72bfae82b89ffdb8f6f1f9eeec91641a5f8fa
Ander Conselvan de Oliveira <ander.conselvan.de.oliveira@intel.com>
2015-09-28 16:17:03 +0300

    drm/i915/tests: Create stubs for building drm_dp_helper.c
    That file will be need by a link training test introduced in a follow up
    commit.

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -23,9 +23,7 @@
 #ifndef _DRM_DP_HELPER_H_
 #define _DRM_DP_HELPER_H_
 
-#include <linux/types.h>
-#include <linux/i2c.h>
-#include <linux/delay.h>
+#include <drm/drm_unit_helper.h>
 
 /*
  * Unless otherwise noted, all values are from the DP 1.1a spec.  Note that
